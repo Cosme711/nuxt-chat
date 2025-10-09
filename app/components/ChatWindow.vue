296ee8dd6ab8941ff5f480ea5007b9b739d19888
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2025-10-09 16:07:45 +0200

    ✨ Add sidebar menu

--- app/components/ChatWindow.vue
+++ app/components/ChatWindow.vue
@@ -78,6 +78,7 @@ watch(() => props.messages, pinToBottom, { deep: true })
 .scroll-container {
   overflow-y: auto;
   height: 100%;
+  width: 100%;
   box-sizing: border-box;
 }
 
--- app/pages/chats/[id].vue
+++ app/pages/chats/[id].vue
@@ -1,25 +1,36 @@
 <script setup lang="ts">
-const route = useRoute();
-const { chat, messages, sendMessage } = useChat(route.params.id as string);
+import type { Chat } from '~/types'
 
-const typing = ref(false);
+const route = useRoute()
+const {
+  chat: chatFromChats,
+  messages,
+  sendMessage,
+} = useChat(route.params.id as string)
+
+if (!chatFromChats.value) {
+  await navigateTo('/', { replace: true })
+}
+const chat = computed(() => chatFromChats.value as Chat)
+
+const typing = ref(false)
 
 const handleSendMessage = async (message: string) => {
-  typing.value = true;
-  await sendMessage(message);
-  typing.value = false;
-};
+  typing.value = true
+  await sendMessage(message)
+  typing.value = false
+}
 
-const appConfig = useAppConfig();
+const appConfig = useAppConfig()
 const title = computed(() =>
   chat.value?.title
     ? `${chat.value.title} - ${appConfig.title}`
     : appConfig.title
-);
+)
 
 useHead({
   title,
-});
+})
 </script>
 
 <template>
